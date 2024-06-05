ebc89a20a7b36937da28c06b780ca79b85338764
Himanshu12866 <manshu010m@gmail.com>
2024-06-05 18:43:48 +0530

    Committed

--- Day-3/script.js
+++ Day-3/script.js
@@ -25,9 +25,22 @@ function CalculateClick(){
     var lblMsg = document.getElementById("lblMsg");
 
     let P = parseInt(document.getElementById("txtAmount").value);
-   
-    lblMsg.innerHTML = P ; 
+    document.getElementById("EnteredAmt").innerHTML = P;
     
+    let N = parseInt(document.getElementById("txtDuration").value) * 12;
+    document.getElementById("EnteredDrn").innerHTML = N;
+let Rate = parseFloat(document.getElementById("txtInterest").value)
+    let R = parseFloat(document.getElementById("txtInterest").value) / 12 / 100;
+    document.getElementById("EnteredRate").innerHTML = Rate;
 
+  
+    
+    var EMI = Math.floor(P * R * (Math.pow(1+R,N)) / (Math.pow(1+R,N))-1)
+    lblMsg.innerHTML = EMI; 
+    document.getElementById("final").innerHTML = EMI.toLocaleString("en-in",{style:"currency", currency:"INR"})
+    
+document.getElementById("detail").style.display = "block";
 }
+
+
 
