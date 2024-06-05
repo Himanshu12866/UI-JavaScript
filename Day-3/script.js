d0ac164ac99ab14f64fe252d4339a3ba9318753b
Himanshu12866 <manshu010m@gmail.com>
2024-06-05 16:54:00 +0530

    Updated

---
+++ Day-3/script.js
@@ -0,0 +1,32 @@
+function ChangeAmount(){
+ document.getElementById("rangeAmount").value =  document.getElementById("txtAmount").value
+ 
+
+}
+function ChangerangeAmount(){
+    document.getElementById("txtAmount").value = document.getElementById("rangeAmount").value
+}
+
+function ChangeDuration(){
+    document.getElementById("rangeDuration").value =  document.getElementById("txtDuration").value
+
+}
+function ChangerangeDuration(){
+    document.getElementById("txtDuration").value = document.getElementById("rangeDuration").value;
+}
+function ChangeInterest(){
+    document.getElementById("rangeInterest").value = document.getElementById("txtInterest").value;
+}
+function ChangerangeInterest(){
+    document.getElementById("txtInterest").value = document.getElementById("rangeInterest").value;
+}
+
+function CalculateClick(){
+    var lblMsg = document.getElementById("lblMsg");
+
+    var P = parseInt(document.getElementById("txtAmount").value);
+   
+    lblMsg.innerHTML = P ; 
+    
+
+}
