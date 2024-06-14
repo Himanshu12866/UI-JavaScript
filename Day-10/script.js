0cc3712a466422823ea59e1a95158477314fdb1f
Himanshu12866 <manshu010m@gmail.com>
2024-06-14 17:23:44 +0530

    Switch Case

--- Day-10/script.js
+++ Day-10/script.js
@@ -30,18 +30,12 @@ function Checkout() {
     document.getElementById("CustomerName").innerHTML = Name;
     document.getElementById("CustomerDate").innerHTML = CustomerDate;
     document.getElementById("CustomerDay").innerHTML = Day;
-   
-
-
     if(Name==""  || CustomerDate == "" || Day == "" || Person =="" || AdvancePay == ""){
         alert("Please Fill The Datails First and Advance Amount To Book Hotel Room")
         document.getElementById("TotalPay").innerHTML = "Pay Advance Amount".fontcolor("red")
-        return ;
-       
+        return ;  
     }
-
     document.getElementById("CustomerPerson").innerHTML = ExtraPerson;
-
     switch (true) {
         case (Person <= 2 && Room.checked && AcRoom.checked || AdvancePay == ""):
             document.getElementById("TotalPay").innerHTML = "Pay Advance Amount".fontcolor("red")
