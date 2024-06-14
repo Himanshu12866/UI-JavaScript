5942ea63d86529f4b215a10b497184d2691bf950
Himanshu12866 <manshu010m@gmail.com>
2024-06-14 16:56:28 +0530

    Completed

--- Day-10/script.js
+++ Day-10/script.js
@@ -22,92 +22,89 @@ function Checkout() {
     var CustomerDate = document.getElementById("InputDate").value;
     var Day = document.getElementById("InputDay").value;
     var Person = document.getElementById("InputPerson").value;
-
+    var ExtraPerson = Person + " ( 1000 &#8377 Per Person)" + " Total:  " + Person * 1000;
     var AdvancePay = document.getElementById("AdvanceAmount").value
-    var Room = document.getElementById("InputDelux")
-    var Aff = document.getElementById("InputSuit")
-    // var Persons = document.getElementById("CustomerPerson")
+    var Room = document.getElementById("InputDelux");
+    var Aff = document.getElementById("InputSuit");
     var AcRoom = document.getElementById("CheckBoxAC");
     document.getElementById("CustomerName").innerHTML = Name;
     document.getElementById("CustomerDate").innerHTML = CustomerDate;
     document.getElementById("CustomerDay").innerHTML = Day;
-    document.getElementById("CustomerPerson").innerHTML = Person;
+    document.getElementById("CustomerPerson").innerHTML = ExtraPerson;
 
-
-    let TotalPeron = Person - 2;
-    let three = (TotalPeron * 1000) * Day;
-    if (Person <= 2 && Room.checked && AcRoom.checked) {
-        RoomCharge = 4000 * Day;
-
-        AcCharge = 1000 * Day;
-        document.getElementById("TotalPay").innerHTML = RoomCharge + AcCharge - AdvancePay + "&#8377";
-        document.getElementById("RoomType").innerHTML = " Luxury Room 4000 &#8377 Per Day";
-        document.getElementById("ACType").innerHTML = " AC Charge &#8377 1000 Per Day";
-
-    }
-    else if (Person <= 2 && (Room.checked || AcRoom.checked)) {
-        RoomCharge = 2400 * Day;
-        // AcCharge = 0
-        if (AcRoom.checked) {
-            AcCharge = 1000 * Day;
-            document.getElementById("TotalPay").innerHTML = RoomCharge + AcCharge - AdvancePay + "&#8377";
-            // document.getElementById("RoomType").innerHTML = "Luxury Room 4000 &#8377 Per Day";
-            document.getElementById("ACType").innerHTML = " A / C Charge &#8377 1000 Per Day";
-        }
-            document.getElementById("ACType").innerHTML = " Non A / C Room";
-            document.getElementById("TotalPay").innerHTML = RoomCharge  - AdvancePay + "&#8377   2 ";
-        if (Room.checked) {
-            RoomCharge = 4000 * Day
-            // text = "Luxury Room :" + 4000 + " Per Day"
-            document.getElementById("RoomType").innerHTML = "Luxury Room &#8377 Per Day"
-            document.getElementById("TotalPay").innerHTML = RoomCharge  -AdvancePay + "&#8377"
-
-        }
-        document.getElementById("TotalPay").innerHTML = RoomCharge  - AdvancePay + " &#8377 ";
-        // document.getElementById("ACType").innerHTML = "A / C Charge &#8377 1000 Per Day"
-        // document.getElementById("RoomType").innerHTML = "Affordable Room 2400 &#8377 Per Day"
-        // document.getElementById("ACType").innerHTML = "Non A / C Room"
-
-    }
-    else if (Person <= 2) {
-        RoomCharge = 2400;
-        document.getElementById("TotalPay").innerHTML = RoomCharge * Day - AdvancePay + "&#8377";
-        document.getElementById("RoomType").innerHTML = " Affordable Room &#8377 2400 Per Day"
-         document.getElementById("ACType").innerHTML = "  Non A / C Room ";
-    }
-    else if (Person > 2 && Room.checked && AcRoom.checked) {
-        RoomCharge = 4000 * Day;
-
-        AcCharge = 1000 * Day;
-        document.getElementById("TotalPay").innerHTML = RoomCharge + AcCharge + three - AdvancePay + "&#8377";
-        // document.getElementById("TotalPay").innerHTML = RoomCharge + AcCharge - AdvancePay;
-        document.getElementById("RoomType").innerHTML = "  Luxury Room 4000 &#8377 Per Day";
-        document.getElementById("ACType").innerHTML = "  A / C Charge &#8377 1000 Per Day";
-    }
-    else if (Person > 2 && (Room.checked || AcRoom.checked)) {
-        RoomCharge = 2400 * Day;
-        AcCharge = 0
-        if (AcRoom.checked) {
-            AcCharge = 1000 * Day
-            document.getElementById("ACType").innerHTML = " A / C Charge &#8377 1000 Per Day";
-
-
-        }
-
-        if (Room.checked) {
-            RoomCharge = 4000 * Day
-
-            document.getElementById("RoomType").innerHTML = " Luxury Room 4000 &#8377 Per Day";
-        }
-        document.getElementById("TotalPay").innerHTML = RoomCharge + AcCharge + three - AdvancePay + "&#8377 ";
-        // document.getElementById("ACType").innerHTML = " 12 Non A / C Room";
-    }
-    else if (Person > 2) {
-        RoomCharge = 2400;
-        document.getElementById("TotalPay").innerHTML = (RoomCharge * Day) + three - AdvancePay + "&#8377";
-        document.getElementById("RoomType").innerHTML = "Affordable Room 2400 &#8377 Per Day";
-        document.getElementById("ACType").innerHTML = "Non A / C Room";
-
+    switch (true) {
+        case (Person <= 2 && Room.checked && AcRoom.checked):
+            document.getElementById("TotalPay").innerHTML = (4000 + 1000 - AdvancePay) * Day + "&#8377";
+            Name.innerHTML = Name;
+            CustomerDate.innerHTML = CustomerDate;
+            Day.innerHTML = Day;
+            Person.innerHTML = Person;
+            document.getElementById("ACType").innerHTML = "1000 &#8377 / Day A / C Room ";
+            document.getElementById("RoomType").innerHTML = "Super Delux Luxuray Room"
+            break;
+        case (Person <= 2 && Room.checked):
+            document.getElementById("TotalPay").innerHTML = (4000 * Day) - AdvancePay + "&#8377";
+            Name.innerHTML = Name;
+            CustomerDate.innerHTML = CustomerDate;
+            Day.innerHTML = Day;
+            Person.innerHTML = Person;
+            document.getElementById("ACType").innerHTML = " Non A / C Room ";
+            document.getElementById("RoomType").innerHTML = "Super Delux Luxuray Room"
+            break;
+        case (Person <= 2 && AcRoom.checked):
+            document.getElementById("TotalPay").innerHTML = (2400 + (1000 * Day)) * Day - AdvancePay + "&#8377";
+            Name.innerHTML = Name;
+            CustomerDate.innerHTML = CustomerDate;
+            Day.innerHTML = Day;
+            Person.innerHTML = Person;
+            document.getElementById("ACType").innerHTML = "1000 &#8377 / Day A / C Room ";
+            document.getElementById("RoomType").innerHTML = "Affordable Room"
+            break;
+        case (Person <= 2):
+            document.getElementById("TotalPay").innerHTML = 2400 * Day - AdvancePay + "&#8377";
+            Name.innerHTML = Name;
+            CustomerDate.innerHTML = CustomerDate;
+            Day.innerHTML = Day;
+            Person.innerHTML = Person;
+            document.getElementById("ACType").innerHTML = " Non A / C Room ";
+            document.getElementById("RoomType").innerHTML = "Affordable Room";
+            break;
+        case (Person > 2 && AcRoom.checked && Room.checked):
+            document.getElementById("TotalPay").innerHTML = (4000 * Day) + (1000 * Day) + (1000 * Day) - AdvancePay + "&#8377";
+            Name.innerHTML = Name;
+            CustomerDate.innerHTML = CustomerDate;
+            Day.innerHTML = Day;
+            Person.innerHTML = ExtraPerson;
+            document.getElementById("ACType").innerHTML = "1000 &#8377 / Day A / C Room ";
+            document.getElementById("RoomType").innerHTML = "Super Luxury Room";
+            break;
+        case (Person > 2 && Room.checked):
+            document.getElementById("TotalPay").innerHTML = (4000 * Day) + (1000 * Day) - AdvancePay + "&#8377";
+            Name.innerHTML = Name;
+            CustomerDate.innerHTML = CustomerDate;
+            Day.innerHTML = Day;
+            Person.innerHTML = ExtraPerson;
+            document.getElementById("ACType").innerHTML = " Non A / C Room ";
+            document.getElementById("RoomType").innerHTML = "Super Luxury Room";
+            break;
+        case (Person > 2 && AcRoom.checked):
+            document.getElementById("TotalPay").innerHTML = (2400 * Day) + (2000 * Day) - AdvancePay + "&#8377";
+            Name.innerHTML = Name;
+            CustomerDate.innerHTML = CustomerDate;
+            Day.innerHTML = Day;
+            Person.innerHTML = ExtraPerson;
+            document.getElementById("ACType").innerHTML = "1000 &#8377 / Day A / C Room ";
+            document.getElementById("RoomType").innerHTML = "Affordable Room";
+            break;
+        case (Person > 2):
+            document.getElementById("TotalPay").innerHTML = (2400 * Day) + (1000 * Day) - AdvancePay + "&#8377";
+            Name.innerHTML = Name;
+            CustomerDate.innerHTML = CustomerDate;
+            Day.innerHTML = Day;
+            Person.innerHTML = ExtraPerson;
+            document.getElementById("ACType").innerHTML = " Non A / C Room ";
+            document.getElementById("RoomType").innerHTML = "Affordable Room";
+            break;
     }
 
 }
