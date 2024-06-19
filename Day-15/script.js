7f0c9dd1693f3ad0ebe8c5b3389ea649083189be
Himanshu12866 <manshu010m@gmail.com>
2024-06-19 14:00:27 +0530

    Uploaded

--- Day-15/script.js
+++ Day-15/script.js
@@ -1,23 +1,33 @@
 function ClickBtn(){
+    document.querySelector("div").innerHTML = "";
     var table =  document.createElement("table");
     table.border = "1";
     table.style.backgroundColor = "grey";
     table.style.color = "white";
     table.style.outline = "1px solid green"
-    var thead = document.createElement("thead");
+    var thead_1 = document.createElement("thead");
     var  tr_1 = document.createElement("tr");
-    var th = document.createElement("th");
-    th.innerHTML = "Details";
-    tr_1.appendChild(th);
-    thead.appendChild(tr_1);
-    table.appendChild(thead);
+    var th_1 = document.createElement("th");
+    th_1.innerHTML = "Name";
+    var th_2 = document.createElement("th");
+    th_2.innerHTML = "Age";
+
+    tr_1.appendChild(th_1);
+    tr_1.appendChild(th_2)
+    thead_1.appendChild(tr_1);
+
+    table.appendChild(thead_1);
+    
 
     // Table Header has been created 
     var tbody = document.createElement("tbody");
     var tr_2 = document.createElement("tr");
     var td = document.createElement("td");
+    var td_1 = document.createElement("td");
     td.innerHTML = "Himanshu Mishra";
     tr_2.appendChild(td);
+    td_1.innerHTML = "22";
+    tr_2.appendChild(td_1)
     tbody.appendChild(tr_2);
     table.appendChild(tbody);
     document.querySelector("div").appendChild(table);
---
+++ Day-16/script.js
@@ -0,0 +1,9 @@
+function ClickMe(){
+    var values = [[10,20,30] , [40,50,60]]
+    for(var value of values){
+        var items = value;
+        for(var item of items){
+            console.log(item);
+        }
+    }
+}
