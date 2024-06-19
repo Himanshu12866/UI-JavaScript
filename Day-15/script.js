8895889c3e7204bce71270f7b83d2be44f2abe93
Himanshu12866 <manshu010m@gmail.com>
2024-06-19 13:35:04 +0530

    .

--- Day-15/script.js
+++ Day-15/script.js
@@ -1,9 +1,22 @@
 function ClickBtn(){
     var table =  document.createElement("table");
     table.border = "1";
-    var theade = document.createElement("thead");
+    var thead = document.createElement("thead");
     var  tr_1 = document.createElement("tr");
     var th = document.createElement("th");
     th.innerHTML = "Details";
-    
+    tr_1.appendChild(th);
+    thead.appendChild(tr_1);
+    table.appendChild(thead);
+
+    // Table Header has been created 
+    var tbody = document.createElement("tbody");
+    var tr_2 = document.createElement("tr");
+    var td = document.createElement("td");
+    td.innerHTML = "Himanshu Mishra";
+    tr_2.appendChild(td);
+    tbody.appendChild(tr_2);
+    table.appendChild(tbody);
+    document.querySelector("div").appendChild(table);
+
 }
