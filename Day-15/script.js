5b00ce5136f9c3dc80b19bc01405b1513779b0a8
Himanshu12866 <manshu010m@gmail.com>
2024-06-19 13:30:41 +0530

    .

--- Day-15/script.js
+++ Day-15/script.js
@@ -1,3 +1,9 @@
 function ClickBtn(){
+    var table =  document.createElement("table");
+    table.border = "1";
+    var theade = document.createElement("thead");
+    var  tr_1 = document.createElement("tr");
+    var th = document.createElement("th");
+    th.innerHTML = "Details";
     
 }
