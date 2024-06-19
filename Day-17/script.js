c974ef291fd1c38ef379a8faafe9a179208f0820
Himanshu12866 <manshu010m@gmail.com>
2024-06-19 14:28:41 +0530

    Uploaded

---
+++ Day-17/script.js
@@ -0,0 +1,19 @@
+function ClickeMe() {
+    document.querySelector("div").innerHTML = `
+     <table style = "border:1px solid red; color:white; background-color:red; margin-top: 20px">
+
+        <thead>
+           <tr>
+               <th>First Name</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Himanshu Mishra</td>
+             </tr>
+        </tbody>        
+    </table>
+    
+    
+    `
+}
