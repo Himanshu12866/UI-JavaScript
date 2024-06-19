85c4e587e840bac0c36a716b9f8e7c958c834d68
Himanshu12866 <manshu010m@gmail.com>
2024-06-19 14:17:21 +0530

    Uploaded

--- Day-15/script.js
+++ Day-15/script.js
@@ -5,10 +5,12 @@ function ClickBtn() {
     table.style.backgroundColor = "grey";
     table.style.color = "white";
     table.style.width = "300px";
-    table.style.textAlign = "left"
-    table.style.marginTop = "20px"
-    table.style.borderRadius = "5px"
+    table.style.textAlign = "left";
+    table.style.marginTop = "20px";
+    table.style.borderRadius = "5px";
 
+    // Create a element using createElement method 
+    
     var thead_1 = document.createElement("thead");
     var tr_1 = document.createElement("tr");
     var th_1 = document.createElement("th");
@@ -16,18 +18,14 @@ function ClickBtn() {
     th_1.innerHTML = "Name";
     var th_2 = document.createElement("th");
     th_2.innerHTML = "Age";
-
     tr_1.appendChild(th_1);
-    tr_1.appendChild(th_2)
+    tr_1.appendChild(th_2);
     thead_1.appendChild(tr_1);
-    table.appendChild(thead_1)
-
+    table.appendChild(thead_1);
 
     // Table Header has been created 
 
     // Now lets create the body of the table 
-
-
 
     var tbody = document.createElement("tbody");
     var tr_2 = document.createElement("tr");
@@ -36,7 +34,7 @@ function ClickBtn() {
     td.innerHTML = "Himanshu Mishra";
     tr_2.appendChild(td);
     td_1.innerHTML = "22";
-    tr_2.appendChild(td_1)
+    tr_2.appendChild(td_1);
     tbody.appendChild(tr_2);
     table.appendChild(tbody);
     document.querySelector("div").appendChild(table);
