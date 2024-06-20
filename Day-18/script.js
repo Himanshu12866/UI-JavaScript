9a846ff2acdf94e3bf3318b9c3b582715b077559
Himanshu12866 <manshu010m@gmail.com>
2024-06-20 16:03:12 +0530

    Uploaded

--- Day-18/script.js
+++ Day-18/script.js
@@ -1,29 +1,26 @@
 
-var ListItem = ["Wake Up", "Brush"];
+var ListItem = [];
 
 
-function LoadList(){
+function LoadList() {
 
-    ListItem.map(function(item){
+    ListItem.map(function (item) {
         var OptionBtn = document.createElement("option");
-        OptionBtn.text =item;
+        OptionBtn.text = item;
         OptionBtn.value = item;
         document.getElementById("SelectList").appendChild(OptionBtn)
-        document.getElementById("InputWrite").innerHTML = ""
-        
-
     })
     document.getElementById("LengthOfList").innerHTML = "Total Task: " + ListItem.length
 }
-
-function BtnAdd(){
+function BtnAdd() {
     let addingItem = document.getElementById("InputWrite").value;
-    if(ListItem.indexOf(addingItem) == -1){
+    if (ListItem.indexOf(addingItem) == -1) {
         ListItem.push(addingItem);
-
+        document.getElementById("InputWrite").innerHTML = ""
         LoadList();
 
     }
-
-
+    else{
+        alert("Task already exist")
+    }
 }
