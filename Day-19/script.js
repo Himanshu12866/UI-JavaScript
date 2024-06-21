d05a66847d7b75a5fe05a55e22c551d5e8499fc1
Himanshu12866 <manshu010m@gmail.com>
2024-06-21 15:06:17 +0530

    Modified

--- Day-19/script.js
+++ Day-19/script.js
@@ -10,18 +10,15 @@ function ClickMe() {
             document.getElementById("ImageId").src = product.image;
             document.getElementById("TitleId").innerHTML = product.title;
             document.getElementById("DescpId").innerHTML = product.description;
-
-            document.getElementById("RateId").innerHTML = `<span class = bi bi-star>${product.rating.rate}</span>`
-            document.getElementById("RatingsId").innerHTML = `<b>${product.rating.ratings}</b> Ratings <b>${product.rating.reviews}</b> Reviews`
-
-
+            document.getElementById("RateId").innerHTML = `<span class = bi bi-star>${product.rating.rate}</span>`;
+            document.getElementById("RatingsId").innerHTML = `<b>${product.rating.ratings}</b> Ratings <b>${product.rating.reviews}</b> Reviews`;
             document.getElementById("PriceId").innerHTML = product.price.toLocaleString('en-in', { style: 'currency', currency: 'INR' });
 
             product.offers.map(function (item) {
                 var li = document.createElement("li");
                 li.classList = "bi bi-tag-fill my-2";
                 li.innerHTML = item;
-                document.getElementById("OffersId").appendChild(li)
+                document.getElementById("OffersId").appendChild(li);
             })
 
 
