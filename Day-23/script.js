ac83970e80efbbb66ffca20d6bf805d2082aec0c
Himanshu12866 <manshu010m@gmail.com>
2024-06-26 17:00:14 +0530

    Modified

--- Day-23/script.js
+++ Day-23/script.js
@@ -20,7 +20,7 @@ function FetchAPI(url) {
          <div class = "card p-2" style = "width: 300px;">
         <div class="text-center">  <img src = " ${data.image}" class=" card-img-top object-fit-contain" style="width:100px;height: 100px"></div>
          <div class = "card-header fw-medium overflow-hidden">
-            <p style="height:30px"> ${data.title}</p>
+            <p style="height:30px;font-size:15px"> ${data.title}</p>
             
             </div>
             <div class = "card-body">
@@ -73,22 +73,42 @@ function VeiwCard() {
     document.getElementById("total").innerHTML = "$" + TotalCost.toFixed(2)
     cardItem.map(function (detail) {
         console.log(detail)
+        var btn = document.createElement("button")
+        btn.innerHTML = 'Delete';
+        btn.className = "btn btn-danger"
+        btn.addEventListener('click', function () {
+            cardItem = cardItem.filter(item => item.id !== detail.id);
+            alert("Your Item is removed from the cart")
+            var BtnPrice = TotalCost - detail.price;
+            TotalCost = BtnPrice;
+            VeiwCard()
+            GetCount()
+
+        })
+
         var tr = document.createElement("tr")
         var td_1 = document.createElement("td")
         var td_2 = document.createElement("td")
         var td_3 = document.createElement("td")
+        var td_4 = document.createElement("td")
 
+        td_4.appendChild(btn)
 
         td_1.innerHTML = detail.title;
+        td_1.style.fontSize = "smaller"
         td_2.innerHTML = `<img style="width:50px; height:50px;" src = ${detail.image}>`;
         td_3.innerHTML = `$ ${detail.price} `;
         tr.appendChild(td_1)
         tr.appendChild(td_2)
         tr.appendChild(td_3)
+        tr.appendChild(td_4)
         document.getElementById("tbody").appendChild(tr)
 
     })
 }
+
+
+
 
 
 
