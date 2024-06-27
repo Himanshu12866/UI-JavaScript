5bc533d1ffc169888a73b501bfe598c74e345836
Himanshu12866 <manshu010m@gmail.com>
2024-06-27 15:16:50 +0530

    Modified

--- Day-24/script.js
+++ Day-24/script.js
@@ -1,16 +1,16 @@
-function msg1(){
+function msg1() {
     alert("Welcome to my website");
 }
-function msg2(){
+function msg2() {
     alert("I hope you will be good")
 }
-function msg3(){
+function msg3() {
     alert("Thnak You Visit Again")
 }
-function bodyLoad(){
-  setTimeout(msg1, 3000);
-  setTimeout(msg2, 8000);
-  setTimeout(msg3, 20000);
+function bodyLoad() {
+    setTimeout(msg1, 3000);
+    setTimeout(msg2, 8000);
+    setTimeout(msg3, 20000);
 
 }
 clearInterval(msg2new)
--- Day-25/script.js
+++ Day-25/script.js
@@ -1,26 +1,25 @@
 let currentSlide = 0;
 
-      function changeSlide(n) {
-        const slides = document.querySelectorAll(".slide");
-        const numSlides = slides.length;
-        currentSlide = (currentSlide + n + numSlides) % numSlides;
-        console.log(currentSlide);
-        document.querySelector(".slides").style.transform = `translateX(-${
-          currentSlide * 100
+function changeSlide(n) {
+    const slides = document.querySelectorAll(".slide");
+    const numSlides = slides.length;
+    currentSlide = (currentSlide + n + numSlides) % numSlides;
+    console.log(currentSlide);
+    document.querySelector(".slides").style.transform = `translateX(-${currentSlide * 100
         }%)`;
-      }
-      function DotClick1() {
-        document.querySelector(
-          ".slides"
-        ).style.transform = `translateX(-${0}%)`;
-      }
-      function DotClick2() {
-        document.querySelector(
-          ".slides"
-        ).style.transform = `translateX(-${100}%)`;
-      }
-      function DotClick3() {
-        document.querySelector(
-          ".slides"
-        ).style.transform = `translateX(-${200}%)`;
-      }
+}
+function DotClick1() {
+    document.querySelector(
+        ".slides"
+    ).style.transform = `translateX(-${0}%)`;
+}
+function DotClick2() {
+    document.querySelector(
+        ".slides"
+    ).style.transform = `translateX(-${100}%)`;
+}
+function DotClick3() {
+    document.querySelector(
+        ".slides"
+    ).style.transform = `translateX(-${200}%)`;
+}
