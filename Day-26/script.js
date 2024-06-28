ae3ba5dc205f4ae3657123282a61b53f1e40f562
Himanshu12866 <manshu010m@gmail.com>
2024-06-28 15:13:09 +0530

    Uploaded

--- Day-26/script.js
+++ Day-26/script.js
@@ -1,45 +1,70 @@
-
-function LoadProduct(id){
-    fetch(`https://fakestoreapi.com/products/${id}`)
-    .then(response => {
-        return response.json();
-    })
-    .then(product => {
-        document.getElementById("lblTitle").innerHTML = product.title;
-        document.getElementById("imgProduct").src = product.image
-    })
-
-}
-function LoadProductAuto(){
+function LoadProductAuto() {
     productId++;
     fetch(`https://fakestoreapi.com/products/${productIt}`)
-    .then(response => {
-        return response.json()
-    })
-    .then(data =>{
-        document.getElementById("lblTitle").innerHTML = data.title;
-        document.getElementById("imgProduct").src = data.image
-    })
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            document.getElementById("lblTitle").innerHTML = data.title;
+            document.getElementById("imgProduct").src = data.image
+        })
+}
+
+function LoadProduct(id) {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(product => {
+            document.getElementById("lblTitle").innerHTML = product.title;
+            document.getElementById("imgProduct").src = product.image
+        })
+
 }
 
 var productId = 1;
-function NextClick(){
+function NextClick() {
+   if(productId <= 19){
     productId++;
-   
+
     LoadProduct(productId)
+    document.getElementById("SeekBar").value = productId;
+   }
+   else{
+   productId = 1;
+   productId++;
+
+    LoadProduct(productId)
+    document.getElementById("SeekBar").value = productId;
+   }
 }
-function PrevClick(){
-    productId--;
+function PrevClick() {
+    if(productId <= 1){
+        productId= 19;
+        productId++;
     LoadProduct(productId)
+    document.getElementById("SeekBar").value = productId;
+    }
+    else{
+        productId--;
+        LoadProduct(productId)
+        document.getElementById("SeekBar").value = productId;
+    }
 }
 
 var timer;
-function PlayBtn(){
+function PlayBtn() {
     document.getElementById("lblStatus").innerHTML = "Sliding started"
     timer = setInterval(NextClick, 3000)
 }
 
-function PauseBtn(){
+function PauseBtn() {
     document.getElementById("lblStatus").innerHTML = "Sliding paused"
     clearInterval(timer)
+}
+function SeekBarChange() {
+    var rangeValue = document.getElementById("SeekBar").value;
+    productId = rangeValue;
+    LoadProduct(productId)
+
 }
