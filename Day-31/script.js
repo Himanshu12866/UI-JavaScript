d1640acf5fe249562507c64196e0136db792ab58
Himanshu12866 <manshu010m@gmail.com>
2024-07-02 15:43:59 +0530

    Modified

--- Day-31/script.js
+++ Day-31/script.js
@@ -19,22 +19,22 @@ function LoadData() {
         i++) {
         var oli_li = document.createElement("li");
         oli_li.innerHTML = values[i].category;
-document.querySelector("ol").appendChild(oli_li)
-for(var j = 0;
-    j < values[i].Products.length;
-    j++
-){
-    var ul = document.createElement("ul");
+        document.querySelector("ol").appendChild(oli_li)
+        for (var j = 0;
+            j < values[i].Products.length;
+            j++
+        ) {
+            var ul = document.createElement("ul");
 
-    var ul_li = document.createElement("li");
-    ul_li.style.color = "green"
-    ul_li.style.listStyle = "square"
-ul_li.innerHTML = values[i].Products[j]
-ul.appendChild(ul_li);
-oli_li.appendChild(ul);
+            var ul_li = document.createElement("li");
+            ul_li.style.color = "green"
+            ul_li.style.listStyle = "square"
+            ul_li.innerHTML = values[i].Products[j]
+            ul.appendChild(ul_li);
+            oli_li.appendChild(ul);
 
 
-}
+        }
     }
 
 
--- Day-32/script.js
+++ Day-32/script.js
@@ -1,7 +1,23 @@
 var n = parseInt(prompt("Enter a value"))
 for(var i = 1; i<=n ; i++){
-    for(var j = 1; j>=i; j++){
-        document.write(" * " + "&nbsp;")
+    for(var j = 1; j<=i; j++){
+        document.getElementById('id1').innerHTML += (" * &nbsp; ")
     }
-    document.write("<br>")
+    document.getElementById('id1').appendChild(document.createElement('br'))
 }
+
+// var n = parseInt(prompt("Enter a value"));
+// var m = " ";
+
+// for (var i = 1; i <= n; i++) {
+//     for(var j = 1; j<=n; j++){
+//         m += " ";
+
+//     }
+//     for(var k = 0; k < 2*1 - 1; k++){
+//         m += "* &nbsp; "
+//     }
+//     m += "\n"
+
+// }
+// document.getElementById("id1").innerHTML = m
