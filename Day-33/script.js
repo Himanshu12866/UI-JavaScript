4c6974789a63277eb7119e95eae110f4f8e94c91
Himanshu12866 <manshu010m@gmail.com>
2024-07-03 14:55:40 +0530

    Modified

--- Day-33/script.js
+++ Day-33/script.js
@@ -1,23 +1,23 @@
-function LoadBtn(){
+function LoadBtn() {
     var inputNumber = document.getElementById("valueNumber").value;
     var valueLength = document.getElementById("valueLength").value;
-  
+
     document.querySelector("p").innerHTML = "";
 
 
-  if(inputNumber <= 1000 && valueLength <= 100){
-    if(inputNumber === "" && valueLength === ""){
-        alert("Please enter all values")
-    }
-    for(var i = 1; i<= valueLength; i++){
-        document.querySelector("p").innerHTML += `<span class="text-danger bi bi-dot" style="font-size:20px"> ${ i * inputNumber}<span> &nbsp`
-        
-        
-    }
-}
+    if (inputNumber <= 1000 && valueLength <= 100) {
+        if (inputNumber === "" && valueLength === "") {
+            alert("Please enter all values")
+        }
+        for (var i = 1; i <= valueLength; i++) {
+            document.querySelector("p").innerHTML += `<span class="text-danger bi bi-dot" style="font-size:20px"> ${i * inputNumber}<span> &nbsp`
 
-    else{
-        
+
+        }
+    }
+
+    else {
+
         alert("Please enter a number between 1 and 1000")
     }
-  }
+}
