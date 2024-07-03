0e76d30a0662576cc81b8c5a176c8fc1f32bf224
Himanshu12866 <manshu010m@gmail.com>
2024-07-03 15:02:05 +0530

    Modified

--- Day-33/script.js
+++ Day-33/script.js
@@ -10,7 +10,7 @@ function LoadBtn() {
             alert("Please enter all values")
         }
         for (var i = 1; i <= valueLength; i++) {
-            document.querySelector("p").innerHTML += `<span class="text-danger bi bi-dot" style="font-size:20px"> ${i * inputNumber}<span> &nbsp`
+            document.querySelector("p").innerHTML += `<div class="text-danger bi bi-dot" style="font-size:20px"> ${i * inputNumber}<div> &nbsp`
 
 
         }
