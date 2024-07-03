26cbfa4cace0e480cc6c230fb51c7abc8f6c5dd4
Himanshu12866 <manshu010m@gmail.com>
2024-07-03 16:41:55 +0530

    Modified

--- Day-34/script.js
+++ Day-34/script.js
@@ -1,12 +1,12 @@
 function LoadClick() {
     document.getElementById("P1").innerHTML = "";
 
-    var rows = parseInt(prompt("Enter Value"));
-    var pattern = "";
+    let rows = parseInt(prompt("Enter Value"));
+    let pattern = "";
     if (rows < 100) {
         for (let i = 1; i <= rows; i++) {
             for (let j = i; j <= i; j++) {
-                pattern += "* &nbsp";
+                pattern += i + " &nbsp";
             }
             document.getElementById("P1").innerHTML += pattern + "<br>";
 
@@ -17,3 +17,34 @@ function LoadClick() {
     }
 }
 
+function LoadClick1() {
+    document.getElementById("P2").innerHTML = "";
+    let rows = parseInt(prompt("Enter Value"));
+    let vari = 1;
+    if (rows < 20) {
+        for (let i = 1; i <= rows; i++) {
+            let pattern = "";
+            for (let j = 1; j <= i; j++) {
+                pattern += vari + "&nbsp";
+                vari++;
+            }
+            document.getElementById("P2").innerHTML += pattern + "<br>";
+
+        }
+
+    }
+    else {
+        alert("Please Enter value less than 20");
+    }
+}
+function LoadClick2() {
+    document.getElementById("P3").innerHTML = "";
+    let rows = parseInt(prompt("Enter Value"));
+    for (let i = 1; i < rows; i++) {
+        let pattern = "";
+        for (let j = 1; j < rows + 1 - i; j++) {
+            pattern += i + "&nbsp";
+        }
+        document.getElementById("P3").innerHTML += pattern + "<br>"
+    }
+}
