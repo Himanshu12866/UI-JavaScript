2a0ce271dc5eb2daa30c272417d02cff198d0849
Himanshu12866 <manshu010m@gmail.com>
2024-07-05 14:38:25 +0530

    Modified

--- Day-35/script.js
+++ Day-35/script.js
@@ -16,11 +16,24 @@ document.write(`Name : ${product[0]};<br> Price : ${product[1]}<br> Ratings : ${
 FName1("IPhone", 80000 , 5.0 , 100);
 
 function FName2(...product){
-    product.map(data => {
-       var load =  document.getElementById("load")
-    load.innerHTML = `<h5> ${data}</>h5`
-   })
-}FName2("Samsung", 1000, 4.5, 10)
- function LoadData(){
-    FName2()
+    document.write(`Name : ${product[0]};<br> Price : ${product[1]}<br> Ratings : ${product[2]}<br> Stoct: ${product[3]}<br>`)
+    //     product.map(data => {
+        //        var load =  document.getElementById("load")
+        //     load.innerHTML = `<h5> ${data[0]}</h5>  `
+        //    })
+    }FName2()
+    function LoadData(){
+    FName2("Samsung", 1000, 4.5, 10)
+ }
+
+ var list = [1,2,3,4]
+ var load = document.getElementById("load")
+
+ function LoadData1(){
+// console.log(load)
+    for(let i =0; i<list.length; i++){
+   
+    //   console.log(list[i])
+        load.innerHTML += `<h5> ${list[i]}</h5><br>  `
+    }
  }
