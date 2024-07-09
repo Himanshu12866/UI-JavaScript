9024057512cf1b7a12edaf1f16b91a238f543e3d
Himanshu12866 <manshu010m@gmail.com>
2024-07-09 14:32:22 +0530

    Modified

--- Day-37/script.js
+++ Day-37/script.js
@@ -1,17 +1,38 @@
-function* Click(){
-yield 10,
-yield "Apple",
-yield "MacBook Air 2 Pro",
-yield "899 $",
-yield ["Delhi", "Mumbai" , "Hyderabad" , "Chennai"]
+function* Click() {
+    yield 1,
+        yield "Apple",
+        yield "MacBook Air 2 Pro",
+        yield "899 $",
+        yield ["Delhi", "Mumbai", "Hyderabad", "Chennai"]
 }
 var list = Click();
-function ClickMe(){
-    console.log(list.next().value);
-console.log(list.next().value);
-console.log(list.next().value);
-console.log(list.next().value);
-console.log(list.next().value);
-console.log(list.next().value);
-}
+function ClickMe() {
+    
+        var id = document.getElementById("result");
+        id.innerHTML = `<table class = "table">
+   
+   <thead>
+   <tr>
+   <th>Id :</th>
+   <th>Name :</th>
+   <th>Modal :</th>
+   <th>Price :</th>
+   <th>Stores :</th>
+   
+   </tr>
+   
 
+   </thead>
+   <tbody>
+   <tr>
+   <td>${list.next().value}</td>
+   <td>${list.next().value}</td>
+   <td>${list.next().value}</td>
+   <td>${list.next().value}</td>
+   <td class = "p-2">${list.next().value}</td>
+   </tr>
+   </tbody>
+
+   
+   </table>`
+    }
