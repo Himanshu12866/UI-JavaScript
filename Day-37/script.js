7a471ec952c9a37c4a8bbed2dd592ff378ff9002
Himanshu12866 <manshu010m@gmail.com>
2024-07-09 14:19:51 +0530

    Modified

--- Day-37/script.js
+++ Day-37/script.js
@@ -1,3 +1,17 @@
-function* GeneratorList(){
-    
+function* Click(){
+yield 10,
+yield "Apple",
+yield "MacBook Air 2 Pro",
+yield "899 $",
+yield ["Delhi", "Mumbai" , "Hyderabad" , "Chennai"]
 }
+var list = Click();
+function ClickMe(){
+    console.log(list.next().value);
+console.log(list.next().value);
+console.log(list.next().value);
+console.log(list.next().value);
+console.log(list.next().value);
+console.log(list.next().value);
+}
+
