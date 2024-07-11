ed5a1ab58f1e95cb8d166cac44d73985e376f9b3
Himanshu12866 <manshu010m@gmail.com>
2024-07-11 15:25:56 +0530

    Modified

---
+++ Day-41/script.js
@@ -0,0 +1,32 @@
+function LoadData() {
+    var name = prompt("Enter Your Name ");
+    var role = prompt("Enter your role", "Admin | User")
+
+
+    class Product {
+        _productName;
+
+
+        get ProductName() {
+            return this._productName;
+        }
+        set ProductName(newValue) {
+
+            this._productName = newValue;
+
+
+        }
+    }
+    let obj = new Product();
+    if (role === "Admin") {
+        obj.ProductName = prompt("Entet New Name")
+    }
+    else {
+        alert(`${name} You are not allowed to change the product name`)
+    }
+    if (obj.ProductName) {
+        document.write(obj.ProductName)
+    }
+
+
+}
