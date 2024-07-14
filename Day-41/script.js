22158d817f19715517d62943a841735eb82adf63
Himanshu12866 <manshu010m@gmail.com>
2024-07-14 22:29:49 +0530

    Modifid

--- Day-41/script.js
+++ Day-41/script.js
@@ -2,19 +2,13 @@ function LoadData() {
     var name = prompt("Enter Your Name ");
     var role = prompt("Enter your role", "Admin | User")
 
-
     class Product {
         _productName;
-
-
         get ProductName() {
             return this._productName;
         }
         set ProductName(newValue) {
-
             this._productName = newValue;
-
-
         }
     }
     let obj = new Product();
@@ -27,6 +21,4 @@ function LoadData() {
     else {
         alert(`${name} You are not allowed to change the product name`)
     }
-
-
 }
