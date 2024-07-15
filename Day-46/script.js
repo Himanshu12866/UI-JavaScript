becdf5d255d9b56f0572abf965257e0a6bfb5902
Himanshu12866 <manshu010m@gmail.com>
2024-07-15 14:10:52 +0530

    Modified

--- Day-46/script.js
+++ Day-46/script.js
@@ -24,8 +24,9 @@ let calc = new Array(new Addition() , new Subtraction() , new Multiplication());
 let operations = prompt("Please Enter Your Operator" , " +, - , * ")
 for(let op of calc){
     if(op.operator == operations){
-        let Number_1 = Number(prompt("Enter First Number"))
-        let Number_2 = Number(prompt("Enter Second Number"))
+        op.Number_1 = parseInt(prompt("Enter Number-1"))
+        op.Number_2 = parseInt(prompt("Enter Number-2"))
         document.querySelector("p").innerHTML = op.print()
+        // console.log(op.print())
     }
 }
