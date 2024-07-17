56b0d1c45271aa2cd8fe63d1a2fc25206d897d40
Himanshu12866 <manshu010m@gmail.com>
2024-07-17 13:49:36 +0530

    Modified

--- Day-49/script.js
+++ Day-49/script.js
@@ -26,3 +26,24 @@ function Media(t) {
 
     }
 }
+function GetData(...product){
+    let div = document.querySelector("div")
+    for(let property in product){
+        div.innerHTML += `${property} : ${product[property]}<br>`
+    }
+}
+var img = ["../images/image-1.jpg" , "../images/image-2.jpg" , "../images/image-3.jpg"]
+function BodyLoad(){
+   img.map(data => {
+    var Img = document.createElement("img");
+    Img.style.width = "100px";
+    Img.style.height = "100px";
+    Img.src = data;
+    Img.addEventListener("mouseover" , (e) => {
+        document.getElementById("Preview").src = data
+    })
+    document.querySelector("nav").appendChild(Img)
+
+   })
+
+}
