24585f1c1acefbfa7cc4988c81d325ec6c21b849
Himanshu12866 <manshu010m@gmail.com>
2024-07-19 14:20:38 +0530

    Modified

--- Day-54/script.js
+++ Day-54/script.js
@@ -1,16 +1,55 @@
 let box = document.getElementById("box");
-let array = ["Window Object" , "Document Object" , "Navigator Object" , "Location Objects" , "History Objects"];
+let array = ["Window Object", "Document Object", "Navigator Object", "Location Objects", "History Objects"];
 
-function LoadObj(){
+function LoadObj() {
     box.innerHTML = ""
-    for(let i = 0; i<array.length; i++){
+    for (let i = 0; i < array.length; i++) {
+        let div = document.createElement("div");
+        div.setAttribute('id', "box-id")
         let ul = document.createElement("ul");
-ul.style.listStyle = "none"
-        let li = document.createElement("li");
-        li.style.fontWeight = "bold"
-       
-        li.innerHTML = (i+1) + " -" + array[i];
+        ul.style.listStyle = "none"
+        let li = document.createElement("button");
+        li.style.fontWeight = "bold";
+
+        li.classList = "btn btn-danger m-2 w-25";
+        li.style.textAlign = "left";
+        let div_1 = document.createElement("div");
+        div_1.setAttribute("id", "div-id");
+        li.addEventListener('click', function () {
+
+            // document.getElementById("div-id").innerHTML = "" 
+
+
+            var WindowObj = ['alert()', "prompt()", 'confirm()', "open()", "print()", "XHttpRequest", "fetch"]
+            if (li.innerHTML === "Window Object") {
+
+                WindowObj.map(item => {
+
+                    div.classList = "m-3";
+
+                    let btn = document.createElement("button");
+                    btn.setAttribute("id", "btn-id")
+                    btn.classList = "btn btn-warning my-2 mx-3 w-25";
+                    btn.innerHTML = item + "<br>";
+                    div_1.appendChild(btn);
+                    document.getElementById("box-id").appendChild(div_1);
+                    let value = document.getElementById("btn-id");
+                    value.addEventListener("click", () => {
+                        if (value.innerHTML = "alert()<br>") {
+                            alert("alert() clicked")
+                        }
+                        else if (value.innerHTML = "prompt()<br>") {
+                            prompt("prompt() clicked")
+                        }
+                    })
+                })
+            }
+        })
+
+        li.innerHTML = array[i];
+
         ul.appendChild(li);
-        box.appendChild(ul);
+        div.appendChild(ul);
+        box.appendChild(div);
     }
 }
