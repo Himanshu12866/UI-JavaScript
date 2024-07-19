1cee9add180c1b418be354325a7847f0c56733f9
Himanshu12866 <manshu010m@gmail.com>
2024-07-19 15:28:54 +0530

    Modified

--- Day-54/script.js
+++ Day-54/script.js
@@ -15,41 +15,75 @@ function LoadObj() {
         li.style.textAlign = "left";
         let div_1 = document.createElement("div");
         div_1.setAttribute("id", "div-id");
+      
         li.addEventListener('click', function () {
-
-            // document.getElementById("div-id").innerHTML = "" 
-
-
-            var WindowObj = ['alert()', "prompt()", 'confirm()', "open()", "print()", "XHttpRequest", "fetch"]
+            var WindowObj = ['alert()', "prompt()", 'confirm()', "open()", "print()", "XHttpRequest", "fetch"];
+            var DocumentObj = ["write()" , "innerHTML" , "innerText" , "getElementById()"]
             if (li.innerHTML === "Window Object") {
-
+                div_1.innerHTML = ""
                 WindowObj.map(item => {
-
                     div.classList = "m-3";
-
                     let btn = document.createElement("button");
                     btn.setAttribute("id", "btn-id")
                     btn.classList = "btn btn-warning my-2 mx-3 w-25";
                     btn.innerHTML = item + "<br>";
                     div_1.appendChild(btn);
                     document.getElementById("box-id").appendChild(div_1);
-                    let value = document.getElementById("btn-id");
-                    value.addEventListener("click", () => {
-                        if (value.innerHTML = "alert()<br>") {
-                            alert("alert() clicked")
-                        }
-                        else if (value.innerHTML = "prompt()<br>") {
-                            prompt("prompt() clicked")
-                        }
-                    })
+                   
+                    btn.addEventListener("click" , LoadFunc)
+                    
                 })
             }
+          else if (li.innerHTML === "Document Object") {
+                div_1.innerHTML = ""
+                DocumentObj.map(item => {
+                    div.classList = "m-3";
+                    let btn = document.createElement("button");
+                    btn.setAttribute("id", "btn-id")
+                    btn.classList = "btn btn-dark my-2 mx-3 w-25";
+                    btn.innerHTML = item + "<br>";
+                    div_1.appendChild(btn);
+                    document.getElementById("box-id").appendChild(div_1);
+                   
+                    btn.addEventListener("click" , LoadFunc)
+                    
+                })
+            }
+            // else if(li.innerHTML = "Document Objects"){
+            //     DocumentObj.map(item => {
+            //         let div_2 = document.createElement("div");
+            //         div_2.setAttribute("id", "div-id-2");
+            //         div.classList = "m-3";
+            //         let btn = document.createElement("button");
+            //         btn.setAttribute("id", "btn-id")
+            //         btn.classList = "btn btn-warning my-2 mx-3 w-25";
+            //         btn.innerHTML = item + "<br>";
+            //         div_2.appendChild(btn);
+            //         document.getElementById("box-id").appendChild(div_2);
+                   
+            //         btn.addEventListener("click" , LoadFunc)
+                    
+            //     })
+            // }
         })
-
         li.innerHTML = array[i];
-
         ul.appendChild(li);
         div.appendChild(ul);
         box.appendChild(div);
     }
+}
+function LoadFunc(arguments) {
+  if(document.getElementById("btn-id").innerHTML == "alert()<br>"){
+    alert("Jia Sri Ram")
+  }
+
+console.log( arguments.target.innerText);
+  if( arguments.target.innerText == "alert()<br>"){
+    let name = prompt("Enter your name")
+    alert("Your name is " + name)
+  }
+  else{
+    alert("Unmatched")
+  }
+
 }
