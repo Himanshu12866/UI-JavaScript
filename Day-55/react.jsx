4ee5f58e7b7b22e29b6e7e99beab5e96c462ba3d
Himanshu12866 <manshu010m@gmail.com>
2024-08-02 13:24:40 +0530

    Added

---
+++ Day-55/react.jsx
@@ -0,0 +1,22 @@
+function Home() {
+    return (
+        <div>
+            <div className="card">
+                <div className="card-header">
+                    <h2>Jai Shri Ram</h2>
+                </div>
+                <div className="car-body">
+                    <p>   
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque distinctio
+                        cupiditate voluptatem, ducimus id perspiciatis. Iusto distinctio suscipit
+                        provident amet veniam quibusdam aliquid perferendis, est totam, in, animi
+                        ab fugit.
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className="btn btn-dark w-50"></button>
+                </div>
+            </div>
+        </div>
+    )
+}
